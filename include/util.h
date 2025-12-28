181bcddae9931334ba40084e394b3d69234eddbd
John Finley <john.finley@gmail.com>
2025-12-28 13:10:46 -0600

    added xip_nocache()

--- include/util.h
+++ include/util.h
@@ -23,3 +23,14 @@ inline bool is_ram(const void *addr)
     return (addr >= (void *)SRAM_BASE) && (addr < (void *)SRAM_END);
 }
 
+
+// convert an XIP address to an XIP address that won't update the cache
+inline const void *xip_nocache(const void *xip_adrs)
+{
+    if (is_xip(xip_adrs)) {
+        uint32_t xip = reinterpret_cast<uint32_t>(xip_adrs);
+        xip = (xip & ~0x0f000000) | 0x03000000;
+        xip_adrs = reinterpret_cast<void *>(xip);
+    }
+    return xip_adrs;
+}
